ead490ee424d86560cdebb52f1e54b2933bc80b4
ssvkart5 <ssvkart5@gmail.com>
2021-02-12 10:43:31 +0530

    vpc terrafrom resource

---
+++ vpc_repo/vpc.tf
@@ -0,0 +1,11 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name  = "ssvkart-vpc"
+    Env   = "Dev"
+    Email = "ssvkart5@gmail.com"
+  }
+  
+}
